d8bc502143373e6dbbf308f2eae72a9303023efe
kai-wei-kfuse <1171113925@qq.com>
2023-03-27 14:17:07 +0800

    123

---
+++ 2023/202303/0327/P2922.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int N = 5e5 + 5;
+vector<int> ve(10005);
+
+struct xx {//字典树
+    int son[2];
+    int x;
+    int tag;
+} tr[N];
+int idx;
+
+void insert(vector<int> a, int len) {  
+    int p = 0;
+    for (int i = 1; i <= len; i++) {
+        int u = a[i];
+        if (!tr[p].son[u]) {
+            tr[p].son[u] = ++idx;
+        }
+        p = tr[p].son[u];
+        tr[p].x++;//这里是统计以p为结尾的字符串的个数
+    }
+    tr[p].tag ++;
+}
+
+int query(vector<int> a, int len) {  
+    int ans = 0;
+    int p = 0;
+    for (int i = 1; i <= len; i++) {
+        int u = a[i];
+        if (!tr[p].son[u]) {
+            break;
+        }
+        p = tr[p].son[u];
+        if (i == len) {
+            ans += tr[p].x;
+        } else
+            ans += tr[p].tag;
+    }
+    return ans;
+}
+
+signed main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        int tmp;
+        cin >> tmp;
+        for (int j = 1; j <= tmp; j++) {
+            cin >> ve[j];
+        }
+        insert(ve, tmp);
+    }
+    for (int i = 1; i <= m; i++) {
+        int tmp;
+        cin >> tmp;
+        for (int j = 1; j <= tmp; j++) {
+            cin >> ve[j];
+        }
+        cout << query(ve, tmp) << endl;
+    }
+}
